e1178fcf53c23e38be63c5f0e4ccac60bcdaf43c
wasghton-Infinit <peckac@gmail.com>
2025-03-28 01:43:38 -0400

    index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+{
+  "name": "engiplus-webhook",
+  "version": "1.0.0",
+  "description": "Servidor de webhook para EngiPlus",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js",
+    "dev": "nodemon index.js"
+  },
+  "dependencies": {
+    "axios": "^1.4.0",
+    "cors": "^2.8.5",
+    "dotenv": "^16.0.3",
+    "express": "^4.18.2"
+  },
+  "devDependencies": {
+    "nodemon": "^2.0.22"
+  },
+  "engines": {
+    "node": ">=16.0.0"
+  }
+}
